7823129743bb2797723485120cc0f8db5c0156e3
Yury Romanov <dot@triplekeys.net>
2021-02-06 02:23:27 +0300

    Cleanup old code

--- lib/yopta_db.ex
+++ lib/yopta_db.ex
@@ -1,27 +1,3 @@
-
-#defmodule YoptaDb.RKI do
-#  use Ecto.Schema
-#  @primary_key {:key}
-#  schema "rki" do
-#    field :key, :text
-#    field :dp_ts, :timestamp
-#    partition_key [:key, :dp_ts]
-#  end
-#end
-
-#defmodule YoptaDb.DP do
-#  use Ecto.Schema
-#
-#  @primary_key {{:key,ts}} 
-#  schema "dp" do
-#    field :key, :text
-#    field :ts, :timestamp
-#    field :data, :blob
-#  end
-#end
-
-  
-
 defmodule YoptaDb do
 
 
@@ -48,12 +24,12 @@ defmodule YoptaDb do
       iex> YoptaDb.get("7dc62817-e855-4568-ac93-d4f0b7f294da",1609892726, 1609892730)
       {:ok, [{1609892726, 21.0},{1609892727", 21.1}]}
 
-  """
-# Tables:
-# rki: [{key, timestamp}] (key index) 
-# dp: [{key, dp_key, offset, data}] (datapoints)
+  ## Tables:
+     rki: [{key, timestamp}] (key index) 
+     dp: [{key, dp_key, offset, data}] (datapoints)
 
-# 
+  """
+
   defp create_keyspace(conn,config) do
     keyspace_st = "CREATE KEYSPACE IF NOT EXISTS #{config[:keyspace]} WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : #{config[:replication_factor]} } "
     IO.puts(keyspace_st)
@@ -71,11 +47,13 @@ defmodule YoptaDb do
     {:ok, conn} = Xandra.start_link(nodes: nodes)
     {:ok, conn} = create_keyspace(conn,keyspace)
   end
+
   defmacro gen_offset(ts) do 
     quote do 
       rem(unquote(ts),18144000)
     end
   end
+
   defmacro gen_dp_ts(ts,offset) do
     quote do
       (unquote(ts) - unquote(offset))
@@ -90,7 +68,8 @@ defmodule YoptaDb do
     {:ok, _} = Xandra.execute(conn, rki_st, %{"key" => key, "dp_ts" => dp_ts})
     dp_st = Xandra.prepare!(conn, "INSERT INTO dp (dp_key, ts, v) VALUES (:dp_key, :ts, :v)")
     {:ok, _} = Xandra.execute(conn, dp_st, %{"dp_key" => dp_key, "ts" => ts, "v" => v})
-  end 
+  end
+
   def get(conn, key,ts1,ts2) do
     rki_st = Xandra.prepare!(conn, "SELECT dp_ts FROM rki WHERE key=:key AND dp_ts >=:ts1 AND dp_ts <= :ts2")
     {:ok, %Xandra.Page{}=dp_tss} = Xandra.execute(conn, rki_st, %{ "key" => key, "ts1" => ts1, "ts2" => ts2})
@@ -98,18 +77,7 @@ defmodule YoptaDb do
      
     dp_st = Xandra.prepare!(conn, "SELECT ts, v FROM dp WHERE dp_key IN :dp_keys AND ts>= :ts1 AND ts<= :ts2")
     {:ok, %Xandra.Page{}=dp_data} = Xandra.execute(conn, dp_st, %{ "dp_keys" => dp_keys, "ts1" => ts1, "ts2" => ts2})
-    Enum.map(dp_data, fn %{ "ts" => ts, "v" => v} -> {ts,v} end)
-
-#    dp_keys = RKI 
-#    |> prepared(key: key, ts1: ts1, ts2: ts2)  
-#    |> select([key, :data])
-#    |> where(key: key, dp_ts: [">=": ts1], dp_ts: ["<=": ts2])
-#    |> RKI.all
-# All the metrics are within the same datapoint
-#    DP
-#    |> prepared(key: dp_keys, ts1: ts1, ts2: ts2)
-#    |> select([:ts, :data])
-#    |> where(key: [in: dp_keys], ts: [">=": ts1], ts: ["<": ts2]) 
-#    |> DP.all
+    {:ok, Enum.map(dp_data, fn %{ "ts" => ts, "v" => v} -> {ts,v} end)}
   end
+
 end
